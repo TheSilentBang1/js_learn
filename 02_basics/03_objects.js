668fb4eb1ea9e0333c5b04213ed50a2db4f2bf84
TheSilentBang1 <113616660+TheSilentBang1@users.noreply.github.com>
2025-03-04 01:49:37 +0000

    xyz

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,7 @@
+// ways to declare objects : literals, constructor
+// when we declare object using literals : not singleton, when using constructor : singleton (one of its kind)
+// Object.create --> constructor, singleton 
+const js_user {
+    name : "arpit",
+    
+}
