78c3068534c6bd7c80d0795ad4c265777a49af3e
TheSilentBang1 <113616660+TheSilentBang1@users.noreply.github.com>
2025-03-07 06:10:55 +0000

    func inc

--- 02_basics/05_objects.js
+++ 02_basics/05_objects.js
@@ -1,1 +1,25 @@
 //object destructuring, json api
+
+//object destructuring : extracting properties from object and assigning them to variables
+
+const course = {
+    course_name : "js course",
+    price: "100"
+}
+
+// how do we access values inside the object : using . operator : nothing wrong w/ it but it can be tedious in case of many values 
+//course.course_name
+
+const {course_name : cname} = course //destructuring the object into variable : if other name for variable not given object value is saved in the key of same name : eg. course_name here --> const {course_name} = course, but we can change the variable in which we want to store destructured object
+console.log(cname);
+
+//menu card ~ documentation of api, api deals in json data in the form of object, fetched api data can be in array form too
+
+//json : defined as object with no name
+//json : javascript object notation
+
+// what an api looks like 
+// {
+//     name : "Arpit",
+//     age : 10
+// }
---
+++ 02_basics/06_functions.js
@@ -0,0 +1,10 @@
+//functions, parameters
+//function : reusable piece of code that performs a specific task
+
+function sayName(){
+    console.log("Arpit");
+}
+// console.log(sayName);
+sayName
+// sayName()
+
